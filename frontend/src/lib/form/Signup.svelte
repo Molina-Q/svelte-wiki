f78d0d3d58214ed8df873efb8fdd642846f3796f
Molina-Q <quentinmolina1@gmail.com>
2024-12-14 23:32:42 +0100

    feat: add Signup form component to frontend

---
+++ frontend/src/lib/form/Signup.svelte
@@ -0,0 +1,20 @@
+<script></script>
+
+<form action="">
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username">
+
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email">
+
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+
+    <label for="confirmPassword">Confirm Password</label>
+    <input type="password" id="confirmPassword" name="confirmPassword">
+
+    <input type="chekcbox" id="terms" name="terms">
+    <label for="terms">I agree to the terms and conditions</label>
+
+    <button type="submit">Sign Up</button>
+</form>
