796a50cb0d87d0657acf9b802a8d629ad9c87a7c
Molina-Q <quentinmolina1@gmail.com>
2024-12-14 23:32:39 +0100

    feat: add navigation component to frontend

---
+++ frontend/src/lib/Nav.svelte
@@ -0,0 +1,12 @@
+<script></script>
+
+<nav>
+    <!-- <img src="" alt=""> -->
+
+    <a href="/">Home</a>
+    <a href="/games">Games</a>
+
+    <input type="search" name="" id="">
+
+    <a href="/login">Login</a>
+</nav>
