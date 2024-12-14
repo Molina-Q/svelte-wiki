15d8f0c6a088e5e4f22835e329ed8fcdfc9fe78a
Molina-Q <quentinmolina1@gmail.com>
2024-12-14 23:36:06 +0100

    feat: add AddGame form component to frontend

---
+++ frontend/src/lib/form/AddGame.svelte
@@ -0,0 +1,20 @@
+<script></script>
+
+<form action="">
+    <label for="title">Title</label>
+    <input type="text" id="title" name="title">
+
+    <label for="description">Description</label>
+    <textarea id="description" name="description"></textarea>
+
+    <label for="image">Image</label>
+    <input type="file" id="image" name="image">
+
+    <label for="price">Price</label>
+    <input type="number" id="price" name="price">
+
+    <label for="platform">Platform</label>
+    <input type="text" id="platform" name="platform">
+
+    <button type="submit">Add Game</button>
+</form>
