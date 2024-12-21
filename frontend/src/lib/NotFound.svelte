213deebcfefae1cb4ada6aefcacba292845e3123
Molina-Q <quentinmolina1@gmail.com>
2024-12-21 16:33:55 +0100

    feat: add NotFound component for 404 error handling

---
+++ frontend/src/lib/NotFound.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<h2 class="title">
+	<span>404</span>&nbsp;Page Not Found
+</h2>
+
+<style>
+	.title {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+
+		font-size: 61px;
+		color: var(--svg-blue);
+	}
+
+	.title span {
+		color: var(--svg-orange);
+		text-shadow: 5px 4px 0 var(--svg-blue);
+	}
+</style>
