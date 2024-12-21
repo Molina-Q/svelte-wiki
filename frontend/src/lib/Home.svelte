fbbc0d18a60ef8e3fb3f6513611193920305c389
Molina-Q <quentinmolina1@gmail.com>
2024-12-21 16:25:08 +0100

    feat: add Home component with welcome message and game search instructions

---
+++ frontend/src/lib/Home.svelte
@@ -0,0 +1,8 @@
+<div>
+    <h1>Home</h1>
+    <p>Welcome to the home page</p>
+    <p>Here you can find all the games we have in our database</p>
+    <p>Use the search bar to find a specific game</p>
+    <p>Or use the navigation bar to find games by platform or developper</p>
+    <p>Enjoy your stay!</p>
+</div>
