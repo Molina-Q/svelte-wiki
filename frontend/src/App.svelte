df7eb28ee3765daebdc131a6d971b45ee2ef5f14
Molina-Q <quentinmolina1@gmail.com>
2024-12-21 16:33:52 +0100

    feat: add NotFound component for handling undefined routes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import Nav from "./lib/Nav.svelte";
-	import AnimatedBackground from "./lib/AnimatedBackground.svelte";
-	import { Link, Route, Router } from "svelte-routing";
-	import Home from "./lib/Home.svelte";
-	import AddGame from "./lib/form/AddGame.svelte";
+  import Nav from "./lib/Nav.svelte";
+  import AnimatedBackground from "./lib/AnimatedBackground.svelte";
+  import { Route, Router } from "svelte-routing";
+  import Home from "./lib/Home.svelte";
+  import AddGame from "./lib/form/AddGame.svelte";
+	import NotFound from "./lib/NotFound.svelte";
 </script>
 
 <main>
@@ -12,6 +13,7 @@
     <AnimatedBackground />
     <Route path="/" component={Home} />
     <Route path="/create/games" component={AddGame} />
+    <Route path="*" component={NotFound} />
   </Router>
 </main>
 
