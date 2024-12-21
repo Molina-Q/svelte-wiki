1cf5081a5bcc305320848917dc9fbb84d4839427
Molina-Q <quentinmolina1@gmail.com>
2024-12-21 16:36:37 +0100

    feat: enhance Home component with styled title and improved layout

--- frontend/src/lib/Home.svelte
+++ frontend/src/lib/Home.svelte
@@ -1,8 +1,15 @@
 <div>
-    <h1>Home</h1>
-    <p>Welcome to the home page</p>
-    <p>Here you can find all the games we have in our database</p>
-    <p>Use the search bar to find a specific game</p>
-    <p>Or use the navigation bar to find games by platform or developper</p>
-    <p>Enjoy your stay!</p>
+	<h1 class="title">Home</h1>
 </div>
+
+<style>
+	.title {
+        margin: 0 auto;
+        text-align: center;
+		font-size: 61px;
+		color: var(--svg-blue);
+
+		color: var(--svg-orange);
+		text-shadow: 5px 4px 0 var(--svg-blue);
+	}
+</style>
